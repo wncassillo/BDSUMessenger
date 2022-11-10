c9583107c3b1757544f24620f4d796fdaecd9b62
wncassillo <cassilloneto@gmail.com>
2022-11-10 20:54:39 -0300

    segundo commit

--- src/bdsmessenger/BDSmessenger.java
+++ src/bdsmessenger/BDSmessenger.java
@@ -1,20 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package bdsmessenger;
+import view.SplashView;
 
-/**
- *
- * @author cassi
- */
 public class BDSmessenger {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+     view.SplashView splash = new SplashView();
+        splash.setVisible(true);
+    
     }
     
 }
